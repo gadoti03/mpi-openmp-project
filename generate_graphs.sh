da4a564b5a8656a69c281c4b88fec75a3ee2eda4
Gabriele <dotigabriele@gmail.com>
2026-01-11 12:47:13 +0100

    deleting

--- generate_graphs.sh
+++
@@ -1,7 +0,0 @@
-sbatch mpi_strong_benchmark.sh
-sbatch mpi_weak_benchmark.sh
-sbatch openMP_strong_benchmark.sh
-sbatch openMP_weak_benchmark.sh
-
-python3 plot.py results/strong_mpi_results.csv results/weak_mpi_results.csv
-python3 plot.py results/strong_openMP_results.csv results/weak_openMP_results.csv
