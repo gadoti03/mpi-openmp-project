873f5f4efb50fe2d0d35ce39104801f537d53c61
Gabriele <dotigabriele@gmail.com>
2026-01-13 17:04:13 +0100

    --ntasks-per-node=2

--- mpi_weak_benchmark.sh
+++ mpi_weak_benchmark.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=tra25_IngInfBo
 #SBATCH --partition=g100_usr_prod
 #SBATCH -t 00:20:00
-#SBATCH --nodes=24
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
-#SBATCH -c 12
+#SBATCH -c 24
 #SBATCH -o job.out
 #SBATCH -e job.err
 
